0fd2fb35e5f4327c92bda1ec8603bf0a87689d01
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2024-01-22 11:23:26 +0600

    added register link in homepage

--- Actions/homePage.php
+++ Actions/homePage.php
@@ -1,51 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <title>Local Business Directory</title>
-    <link rel="stylesheet" href="../CSS/home.css">
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <title>Local Business Directory</title>
+  <link rel="stylesheet" href="../CSS/home.css">
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </head>
+
 <body>
 
-<header>
-     <h2 class="logo">Logo</h2>
-     <nav class="navigation">
-        <a href="dashboard.html">Dashboard</a>
-        <a href="Inventory.html">Inventory</a>
-        <a href="Bill.html">Bill</a>
-        <a href="Customer.html">Customers</a>
-        <a href="Profile.html">Profile</a>
-        <button class="btnLogin-popup">Login</button>
-</nav>
-</header>
+  <header>
+    <h2 class="logo">Logo</h2>
+    <nav class="navigation">
+      <a href="dashboard.html">Dashboard</a>
+      <a href="Inventory.html">Inventory</a>
+      <a href="Bill.html">Bill</a>
+      <a href="Customer.html">Customers</a>
+      <a href="Profile.html">Profile</a>
+      <button class="btnLogin-popup">Login</button>
+    </nav>
+  </header>
 
-<div class="wrapper">
- <div class="form-box login">
-   <h2>Login</h2>
-     <form action="#">
-      <div class="input-box">
-   <span class="icon"><ion-icon name="mail-outline"></ion-icon></span>
-   <input type="email" required>
-   <label>Email</labe1>
-      </div>
-      
-<div class="input-box">
-  <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></span>
-  <input type="password" required>
-   <label>Password</label >
+  <div class="wrapper">
+
+    <div class="form-box login">
+      <h2>Login</h2>
+      <form action="" method="post">
+        <div class="input-box">
+          <span class="icon"><ion-icon name="mail-outline"></ion-icon></span>
+          <input type="email" required>
+          <label>Email</labe1>
+        </div>
+
+        <div class="input-box">
+          <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></span>
+          <input type="password" required>
+          <label>Password</label>
+        </div>
+
+        <div class="remember-forgot">
+          <a href="#">Forgot Password?</a>
+        </div>
+
+        <button type="submit" class="btn">Login</button>
+
+      </form>
+
+      <div class="register-link">
+      <a href="register.php" class="register-link"> Don't have an account?</a>
+    </div>
     </div>
 
-    <div class="remember-forgot">
-<a href="#">Forgot Password?</a>
-</div>
+   
 
-    <button type="submit" class="btn">Login</button>
-<div class="login-register">
-</p> Don't have an account ? <a href="#" class="register-link"></a></p>
-</form>
-</div>
+  </div>
 </body>
+
 </html>
