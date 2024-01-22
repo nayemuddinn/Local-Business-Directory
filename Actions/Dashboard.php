4268c8c4f3b7f12cf3f12f6db9982c33d4d42fd5
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2024-01-22 18:52:40 +0600

    Created dashboard

---
+++ Actions/Dashboard.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <title>Homepage</title>
+  <link rel="stylesheet" href="../CSS/login.css">
+  <link rel="stylesheet" href="../CSS/home.css">
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</head>
+
+<body>
+
+  <header>
+    <h2 class="logo">Logo</h2>
+    <nav class="navigation">
+      <a href="dashboard.html">Dashboard</a>
+      <a href="Inventory.html">Inventory</a>
+      <a href="Bill.html">Bill</a>
+      <a href="Customer.html">Customers</a>
+      <a href="Profile.html">Profile</a>
+      <button class="btnLogin-popup">Log out</button>
+    </nav>
+  </header>
+
+  <div class="home-wrapper">
+    <p>Welcome to local business directory website</p>
+  </div>
+
+</body>
+
+</html>
--- Actions/Login.php
+++ Actions/Login.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <title>Local Business Directory</title>
-  <link rel="stylesheet" href="../CSS/home.css">
+  <link rel="stylesheet" href="../CSS/login.css">
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </head>
