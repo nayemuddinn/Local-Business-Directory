07ef2b8522d39ddf9f12e880b580dd52b82875a9
nayemuddinn <126597905+nayemuddinn@users.noreply.github.com>
2024-01-26 12:36:45 +0600

    created table in search page and connected with the database

--- Actions/Searchproduct.php
+++ Actions/Searchproduct.php
@@ -7,6 +7,7 @@
     <title>Inventory</title>
     <link rel="stylesheet" href="../CSS/home.css">
     <link rel="stylesheet" href="../CSS/Inventory.css">
+    <link rel="stylesheet" href="../CSS/tableStyles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
@@ -46,6 +47,48 @@
                     <button type="submit" class="searchButton">
                         <i class="fa fa-search"></i>
                     </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <h1 class="table-title">Products</h1>
+
+            <div style="margin-left=200px;">
+                <div class="table-content">
+                    <?php
+                    include 'db_connection.php';
+                    $sql = "SELECT * FROM inventory";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                        echo "<table border='0'>
+                            <th>productID</th>
+                            <th align='center'>productName</th>
+							<th align='center'>productCategory</th>
+							<th align='center'>price</th>
+							<th align='center'>available</th>
+							<th align='center'>unit</th>
+                        </tr>";
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<tr>
+                            <td>" . $row["productID"] . " " . "</td>
+                            <td>" . $row["productName"] . " " . "</td>
+							<td>" . $row["productCategory"] . " " . "</td>
+							<td>" . $row["price"] . " " . "</td>
+							<td>" . $row["available"] . " " . "</td>
+							<td>" . $row["unit"] . " " . "</td>
+                            
+                        </tr>";
+                        }
+
+                        echo "</table>";
+                    } else {
+                        echo "0 results";
+                    }
+
+                    $conn->close();
+                    ?>
                 </div>
             </div>
         </div>
